8a6f2237ce33119d40bf466d3d70cc9644e033f6
Jak Kos <45759112+jakkosdev@users.noreply.github.com>
2023-02-09 08:50:42 +0000

    just use an mpsc channel to change the function

--- sonars/src/main.rs
+++ sonars/src/main.rs
@@ -1,5 +1,6 @@
-use rodio::{ Source};
+use rodio::{ Source, queue};
 use rodio::{OutputStream, Sink};
+use std::sync::mpsc::{channel, Receiver};
 use std::{
 
     f32::consts::PI,
@@ -41,93 +42,53 @@ fn main() {
 
     // Add a dummy source of the sake of the example.
     let start_time = Instant::now();
-    let samples: Arc<Samples> = Default::default();
+    
+
+    
+    
+    let tau = 2.0 * PI;
+    let hz = 440.0;
+    let sample_func = Box::new(move |t:f32|{
+
+        let outer_wave = (t * tau).sin().abs();
+        let outer_wave2 = (t * tau + tau*0.25).sin().abs();
+        let inner_wave =  |a:f32| (t * tau * a).sin().round();
+        let detune = |func: &dyn Fn(f32) -> f32, input:f32, detune:f32|  (func(input - detune) + func(input) + func(input + detune))/3.0;
+        let res = outer_wave * detune(&inner_wave, 440.0, 1.2) * outer_wave2;
+        res.clamp(-1.0, 1.0)
+    });
+
+    let (tx,rx) = channel::<Box<dyn Fn(f32) -> f32 + Send>>();
     let source = SamplesSource {
-        samples: samples.clone(),
-        t: (Instant::now() - start_time).as_secs_f32(),
+        sample_func,
+        new_sample_func: rx,
+        t: 0,
     };
     // let source = SineWave::new(440.0);
     sink.append(source.amplify(0.3));
 
-    
-    let tau = 2.0 * PI;
-    let hz = 440.0;
-    samples
-        .try_set(Box::new(move |t|{
+    sleep(Duration::from_secs(3));
 
-            let outer_wave = (t * tau).sin().abs();
-            let outer_wave2 = (t * tau + tau*0.25).sin().abs();
-            let inner_wave =  |a:f32| (t * tau * a).sin().round();
-            let detune = |func: &dyn Fn(f32) -> f32, input:f32, detune:f32|  (func(input - detune) + func(input) + func(input + detune))/3.0;
-            let res = outer_wave * detune(&inner_wave, 440.0, 1.2) * outer_wave2;
-            res.clamp(-1.0, 1.0)
-        })).unwrap();
+    let sample_func = Box::new(move |t:f32|{
+
+        let outer_wave = (t * tau).sin().abs();
+        let outer_wave2 = (t * tau + tau*0.25).sin().abs();
+        let inner_wave =  |a:f32| (t * tau * a).sin().round();
+        let detune = |func: &dyn Fn(f32) -> f32, input:f32, detune:f32|  (func(input - detune) + func(input) + func(input + detune))/3.0;
+        let res = outer_wave * detune(&inner_wave, 660.0, 1.0) * outer_wave2;
+        res.clamp(-1.0, 1.0)
+    });
+
+    tx.send(sample_func).ok();
 
     loop{
         sleep(Duration::from_secs(1))
     }
 }
-pub struct Samples {
-    buffer_a: Mutex<Box<dyn Fn(f32) -> f32 + Send>>,
-    buffer_b: Mutex<Box<dyn Fn(f32) -> f32 + Send>>,
-    a_is_read: AtomicBool,
-    pending_switch: AtomicBool,
-}
-
-impl Default for Samples {
-    fn default() -> Self {
-        Self {
-            buffer_a: Mutex::new(Box::new(|_| 0.0)),
-            buffer_b: Mutex::new(Box::new(|_| 0.0)),
-            a_is_read: AtomicBool::new(true),
-            pending_switch: AtomicBool::new(false),
-        }
-    }
-}
-
-impl Samples {
-    pub fn read(&self, t: f32) -> f32 {
-        if self.pending_switch.load(Ordering::Relaxed) {
-            self.a_is_read.fetch_xor(true, Ordering::Relaxed);
-            self.pending_switch.store(false, Ordering::Relaxed);
-        }
-
-        let buffer = if self.a_is_read.load(Ordering::Relaxed) {
-            self.buffer_a.lock().unwrap()
-        } else {
-            self.buffer_b.lock().unwrap()
-        };
-
-        buffer(t)
-    }
-
-    pub fn try_set(
-        &self,
-        f: Box<dyn Fn(f32) -> f32 + Send>,
-    ) -> Result<(), TryLockError<MutexGuard<dyn Fn(f32) -> f32 + Send>>> {
-        if !self.pending_switch.load(Ordering::Relaxed) {
-            if self.a_is_read.load(Ordering::Relaxed) {
-                if let Ok(mut buffer) = self.buffer_b.try_lock() {
-                    self.pending_switch.store(true, Ordering::Relaxed);
-                    *buffer = f;
-                    return Ok(())
-                }
-            } else {
-                if let Ok(mut buffer) = self.buffer_a.try_lock() {
-                    self.pending_switch.store(true, Ordering::Relaxed);
-                    *buffer = f;
-                    return Ok(())
-                }
-            }
-        }
-
-        Err(TryLockError::WouldBlock)
-    }
-}
-
 pub struct SamplesSource {
-    samples: Arc<Samples>,
-    t: f32,
+    sample_func: Box<dyn Fn(f32) -> f32 + Send>,
+    new_sample_func: Receiver<Box<dyn Fn(f32) -> f32 + Send>>,
+    t: usize
 }
 
 impl Source for SamplesSource {
@@ -152,8 +113,12 @@ impl Iterator for SamplesSource {
     type Item = f32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let f = self.samples.read(self.t);
-        self.t += INV_SAMPLE_RATE;
+
+        if let Ok(new_sample_func) = self.new_sample_func.try_recv(){
+            self.sample_func = new_sample_func
+        }
+        let f = (self.sample_func)(self.t as f32  * INV_SAMPLE_RATE);
+        self.t += 1;
         Some(f)
     }
 }

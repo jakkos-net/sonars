197d0dd712d65c74cb5344f996816bcad435aa92
Jak Kos <45759112+jakkosdev@users.noreply.github.com>
2023-02-21 20:29:53 +0000

    web audio minimum working example

--- sonars/src/main.rs
+++ sonars/src/main.rs
@@ -1,53 +1,49 @@
-pub mod sound;
 pub mod lang;
+pub mod sound;
 
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContext, EguiPlugin};
 use lang::Program;
 use sound::{SoundControl, SoundPlugin};
-use wasm_bindgen::JsValue;
 
 fn main() {
-
     App::new()
-
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
         .add_plugin(SoundPlugin)
-
         .init_resource::<CodeEditorData>()
-
         .add_system(coding_ui)
-
         .run();
-
 }
 
 #[derive(Resource)]
-struct CodeEditorData{
+struct CodeEditorData {
     pub src: String,
-    pub error_text: String, 
+    pub error_text: String,
 }
 
-impl Default for CodeEditorData{
+impl Default for CodeEditorData {
     fn default() -> Self {
-        Self{
+        Self {
             src: "Woo I'm some source code... Apparently...".into(),
-            error_text: "".into()
+            error_text: "".into(),
         }
     }
 }
 
-fn coding_ui(mut egui_context: ResMut<EguiContext>, mut editor_data: ResMut<CodeEditorData>, sound : NonSend<SoundControl>) {
+fn coding_ui(
+    mut egui_context: ResMut<EguiContext>,
+    mut editor_data: ResMut<CodeEditorData>,
+    mut sound: NonSendMut<SoundControl>,
+) {
     egui::Window::new("Editor").show(egui_context.ctx_mut(), |ui| {
-        if ui.text_edit_multiline(&mut editor_data.src).changed(){
-            editor_data.error_text = match Program::from_str(&editor_data.src){
+        if ui.text_edit_multiline(&mut editor_data.src).changed() {
+            editor_data.error_text = match Program::from_str(&editor_data.src) {
                 Ok(p) => {
-
                     sound.set(p.to_fn().unwrap());
                     "Compilation succesful!".into()
-                },
-                Err(e) => e.to_string()
+                }
+                Err(e) => e.to_string(),
             }
         };
         ui.label(&editor_data.error_text);
--- sonars/src/sound.rs
+++ sonars/src/sound.rs
@@ -1,6 +1,8 @@
-use bevy::prelude::{Plugin};
+use std::f32::consts::PI;
 
-use web_sys::{AudioBufferSourceNode, AudioContext, AudioBuffer};
+use bevy::prelude::Plugin;
+
+use web_sys::{AudioBufferSourceNode, AudioContext};
 
 const SAMPLE_RATE: u32 = 48_000;
 const INV_SAMPLE_RATE: f32 = 1.0 / (SAMPLE_RATE as f32);
@@ -10,42 +12,44 @@ pub struct SoundPlugin;
 
 pub type SoundFn = Box<dyn Fn(f32) -> f32 + Send + Sync>;
 
-impl Plugin for SoundPlugin{
+impl Plugin for SoundPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-
-        let ctx = web_sys::AudioContext::new().unwrap();
-
-        // Create our web audio objects.
-        let gain = ctx.create_gain().unwrap();
-    
-        
-    
-        let buffer_node = ctx.create_buffer_source().unwrap();
-        buffer_node.set_loop(true);
-    
-        // Some initial settings:
-        gain.gain().set_value(0.2); // starts muted
-        buffer_node.connect_with_audio_node(&gain).unwrap();
-        gain.connect_with_audio_node(&ctx.destination()).unwrap();
-        buffer_node.start().unwrap();
-        let buffer = ctx.create_buffer(1, BUFFER_SIZE, SAMPLE_RATE as f32).unwrap();
-        let data =(0..BUFFER_SIZE).into_iter().map(|x| ((x as f32) * INV_SAMPLE_RATE * 2.0 * 3.141592 * 440.0).sin()).collect::<Vec<_>>();
-        buffer.copy_to_channel(data.as_slice(), 0).unwrap();
-        buffer_node.set_buffer(Some(&buffer));
-        app.insert_non_send_resource(SoundControl{ctx, buffer_node, buffer});
+        let mut sound_control = SoundControl { ctx: None };
+        sound_control.set(Box::new(move |t| (t * 440.0 * 2.0 * PI).sin()));
+        app.insert_non_send_resource(sound_control);
     }
 }
 
-
-pub struct SoundControl{
-    ctx: AudioContext,
-    buffer_node: AudioBufferSourceNode,
-    buffer: AudioBuffer,
+pub struct SoundControl {
+    ctx: Option<AudioContext>,
 }
 
-impl SoundControl{
-    pub fn set(&self, sound_fn: SoundFn){
-        let data =(0..BUFFER_SIZE).into_iter().map(|x| (sound_fn((x as f32) * INV_SAMPLE_RATE))).collect::<Vec<_>>();
-        self.buffer.copy_to_channel(data.as_slice(), 0).unwrap();
+impl SoundControl {
+    pub fn set(&mut self, sound_fn: SoundFn) {
+        let data = (0..BUFFER_SIZE)
+            .into_iter()
+            .map(|x| (sound_fn((x as f32) * INV_SAMPLE_RATE)))
+            .collect::<Vec<_>>();
+
+        let ctx = web_sys::AudioContext::new().unwrap();
+        let gain = ctx.create_gain().unwrap();
+        let buffer_node = ctx.create_buffer_source().unwrap();
+        buffer_node.set_loop(true);
+        gain.gain().set_value(0.2);
+        buffer_node.connect_with_audio_node(&gain).unwrap();
+        gain.connect_with_audio_node(&ctx.destination()).unwrap();
+        buffer_node.start().unwrap();
+        let buffer = ctx
+            .create_buffer(1, BUFFER_SIZE, SAMPLE_RATE as f32)
+            .unwrap();
+        buffer.copy_to_channel(data.as_slice(), 0).unwrap();
+
+        if let Some(old_ctx) = &self.ctx {
+            old_ctx.close().ok();
+        }
+
+        buffer_node.set_buffer(Some(&buffer));
+
+        self.ctx = Some(ctx);
     }
 }

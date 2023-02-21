724b6925eebcea0cfc9d50db5865d6faf56f555b
Jak Kos <45759112+jakkosdev@users.noreply.github.com>
2023-02-21 21:08:38 +0000

    wip lang

--- sonars/src/lang.rs
+++ sonars/src/lang.rs
@@ -4,57 +4,47 @@ use thiserror::Error;
 
 use crate::sound::SoundFn;
 
-enum Expression{
-    Sin(E,E),
-    Add(E,E),
-    Mul(E,E),
-    Div(E,E),
-    Sub(E,E),
+enum Expression {
+    Sin(E, E),
+    Add(E, E),
+    Mul(E, E),
+    Div(E, E),
+    Sub(E, E),
     Mod(E, E),
-    Pow(E,E),
+    Pow(E, E),
     Num(f32),
     Invoke(String),
-    Time
+    Time,
 }
 type E = Box<Expression>;
 
-enum Statement{
-    Assign(String, Vec<String>, Expression)
+pub struct Program {
+    expr: Expression,
 }
 
-pub struct Program{
-    hz: f32
-}
-
-impl Program{
-
-    pub fn from_str(src: &str) -> Result<Self, ParsingError>
-    {
-        if let Ok(hz) = src.parse::<f32>(){
-            Ok(Program{hz})
+impl Program {
+    pub fn from_str(src: &str) -> Result<Self, ParsingError> {
+        if let Ok(hz) = src.parse::<f32>() {
+            Ok(Program { hz })
         } else {
             Err(ParsingError::Unknown)
         }
     }
 
-    pub fn to_fn(&self) -> Result<SoundFn, CompilationError>
-    {
+    pub fn to_fn(&self) -> Result<SoundFn, CompilationError> {
         let hz = self.hz;
-        Ok(Box::new(
-            move |t| (t * hz * 2.0 * PI).sin()
-        ))
+        Ok(Box::new(move |t| (t * hz * 2.0 * PI).sin()))
     }
 }
 
-
 #[derive(Error, Debug)]
-pub enum ParsingError{
+pub enum ParsingError {
     #[error("Unknown parsing error")]
-    Unknown
+    Unknown,
 }
 
 #[derive(Error, Debug)]
-pub enum CompilationError{
+pub enum CompilationError {
     #[error("Unknown compilation error")]
-    Unknown
+    Unknown,
 }

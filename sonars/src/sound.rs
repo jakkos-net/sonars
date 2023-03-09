1c665a7c233c1746e35f3b645c3df2e786b2ab2f
jakkosdev <45759112+jakkosdev@users.noreply.github.com>
2023-03-09 08:28:02 +0000

    remove now unneeded sound stuff

--- sonars/src/sound.rs
+++ sonars/src/sound.rs
@@ -35,9 +35,7 @@ impl Plugin for SoundPlugin {
 
 fn update_sound(
     mut sound_control: ResMut<SoundControl>,
-    mut sound_resources: NonSendMut<SoundResources>,
 ) {
-    sound_resources.update(&sound_control.next_fn).unwrap();
     sound_control.update();
 }
 
@@ -78,10 +76,7 @@ pub fn push_sound(new_fn: SoundFn){
 
 struct SoundResources {
     pub ctx: AudioContext,
-    pub gain: GainNode,
-    pub buf: Option<AudioBufferSourceNode>,
     pub time_start: f64,
-    pub next_buf: Option<AudioBufferSourceNode>,
 }
 
 impl Default for SoundResources {
@@ -89,9 +84,6 @@ impl Default for SoundResources {
         cfg_if::cfg_if! {
             if #[cfg(target_arch = "wasm32")] {
                 let ctx = AudioContext::new().unwrap();
-                let gain = ctx.create_gain().unwrap();
-                gain.gain().set_value(GAIN);
-                gain.connect_with_audio_node(&ctx.destination()).unwrap();
             } else {
 
                 let ctx = AudioContext::new(AudioContextOptions {
@@ -100,102 +92,16 @@ impl Default for SoundResources {
                     sink_id: "".into(),
                     render_size_hint: web_audio_api::context::AudioContextRenderSizeCategory::Default,
                 });
-
-                setup_desktop_worklet(&ctx);
-
-                let gain = ctx.create_gain();
-                gain.connect(&ctx.destination());
-                gain.gain().set_value(GAIN);
             }
         }
+
+        setup_worklet(&ctx);
 
         let time_start = ctx.current_time();
         Self {
             ctx,
-            gain,
-            buf: None,
-            next_buf: None,
             time_start: time_start,
         }
-    }
-}
-
-const GAIN: f32 = 0.2;
-
-impl SoundResources {
-    pub fn update(&mut self, sound_fn: &SoundFn) -> anyhow::Result<()> {
-        // have we been playing the current buffer for too long?
-        let t = self.ctx.current_time();
-        if (t - self.time_start) > BUFFER_TIME_LENGTH {
-            cfg_if::cfg_if! {
-                if #[cfg(target_arch = "wasm32")] {
-                    let new_buf = self.ctx.create_buffer_source().unwrap();
-                    new_buf.set_loop(true);
-                    new_buf.connect_with_audio_node(&self.gain).unwrap();
-                    // new_buf.start().unwrap();
-                    let buf_data = &self.ctx
-                        .create_buffer(1, BUFFER_SIZE, SAMPLE_RATE as f32)
-                        .unwrap();
-                    buf_data.copy_to_channel(data.as_slice(), 0).unwrap();
-
-                    if self.buf.is_some() {
-                        let old_buf = self.buf.take().unwrap();
-                        // old_buf.stop().ok();
-                        old_buf.disconnect().ok();
-                        drop(old_buf);
-                    }
-
-                    new_buf.set_buffer(Some(&buf_data));
-                } else {
-
-                    // CURRENT SOUND
-                    // #############
-
-                    // if we were playing sound before, stop
-                    if self.buf.is_some() {
-                        let old_buf = self.buf.take().unwrap();
-                        // old_buf.stop();
-                        old_buf.disconnect();
-                        drop(old_buf);
-                    }
-
-                    // FOR NEXT TIME
-                    // #############
-
-                    // what time will the next buffer start playing
-                    let next_t = self.time_start + BUFFER_TIME_LENGTH;
-                    self.time_start = next_t;
-                    let next_t_f32 = next_t as f32;
-                    // generate the data for the next buffer
-                    let data = (0..BUFFER_SIZE)
-                    .into_iter()
-                    .map(|x| (sound_fn)((x as f32) * INV_SAMPLE_RATE + next_t_f32))
-                    .collect::<Vec<_>>();
-
-                    // create the buffer_node
-                    let new_buf = self.ctx.create_buffer_source();
-                    // connect it to the sound ouput
-                    new_buf.connect(&self.gain);
-
-                    // create the buffer
-                    let mut buf_data = self.ctx
-                        .create_buffer(1, BUFFER_SIZE as usize, SAMPLE_RATE as f32);
-                    // copy the data into the buffer
-                    // todo_major why do we have to make a vec, then copy it? Seems inefficient
-                    buf_data.copy_to_channel(data.as_slice(), 0);
-                    // put the buffer in the buffer node
-                    new_buf.set_buffer(buf_data);
-
-                    // new_buf.start_at(next_t);
-
-
-                    // move the now playing buffer into buf, and the for-next-time buffer into next_buf
-                    self.buf = self.next_buf.take();
-                    self.next_buf = Some(new_buf);
-                }
-            }
-        }
-        Ok(())
     }
 }
 
@@ -206,7 +112,7 @@ static CURRENT_SOUND_FN: Lazy<Mutex<SoundFn>> = Lazy::new(|| {
     Mutex::new(Box::new(|_| 0.0))
 });
 
-fn setup_desktop_worklet(context: &AudioContext){
+fn setup_worklet(context: &AudioContext){
     let noise = MyNode::new(context);
     noise.connect(&context.destination());
 }

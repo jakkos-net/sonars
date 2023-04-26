2e78f08809a9b6b15dd66e9fcd032e6ad2a03dd2
jakkosdev <me@jakkos.dev>
2023-04-26 19:34:28 +0100

    nix compat

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "rust devshell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.vulkan-loader ];
+
+          buildInputs = [
+            cmake
+            pkgconfig
+            glib
+            fontconfig
+            atk
+            gtk3
+            alsa-lib
+            systemd
+            openssl
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            })
+            just
+          ];
+        };
+      }
+    );
+}
